a7f812b047edaf92cb138f44ee047b387cbf6f68
bialek-k <krzysztof.bialek1@gmail.com>
2023-09-27 14:45:22 +0200

    feat/get-user-data

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,20 +1,12 @@
-const User = require('../models/user')
+const User = require("../models/user");
 
-exports.login = (req,res,next) => {
-  const username = req.body.username; 
-  const password = req.body.password
+exports.login = async (req, res, next) => {
+	const username = req.body.username;
+	const password = req.body.password
 
-  User.findOne({username: username, password: password}, (err, user) => {
-    if(err){
-      console.log(err);
-      return res.status(500).send();
-    }
+	const userData = await User.findOne({username:username, password:password}).then(user => console.log(user))
+	console.log(userData);
 
-    if(!user){
-      return res.status(404).send();
-    }
+	return res.status(200).send("good")
 
-    return res.status(200).send()
-  })
-
-}
+};
--- index.js
+++ index.js
@@ -2,12 +2,19 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
+const bodyParser = require('body-parser')
 
 const mongoose = require("mongoose");
 
 const authRoutes = require('./routes/auth')
-app.use(authRoutes)
 
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  }),
+);
+app.use(authRoutes)
 
 mongoose
 	.connect(
