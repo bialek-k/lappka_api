0a96c42b192496ca9eaa5e8693bc0fb5a5e34ad4
Krzysztof Białek <krzysztof.bialek1@gmail.com>
2023-10-08 17:02:01 +0200

    feat/add refresh token handler

--- controllers/auth.js
+++ controllers/auth.js
@@ -18,12 +18,37 @@ exports.login = async (req, res, next) => {
     }
 
 		const accessToken = jwt.sign({id:existingUser._id}, process.env.NODE_JWT_ACCESSTOKEN_KEY, {expiresIn:86400}); 
-		const refreshToken = jwt.sign({id:existingUser._id}, process.env.NODE_JWT_REFRESHTOKEN_KEY, {expiresIn:56200}); 
-
+		const refreshToken = jwt.sign({id:existingUser._id}, process.env.NODE_JWT_REFRESHTOKEN_KEY, {expiresIn:525600}); 
+		
     return res.status(200).send({accessToken, refreshToken});
-
+		
 	} catch (err){
 		return res.status(400).send('Internal Server Error')
 	}
-
+	
 };
+
+exports.refreshToken = async (req,res, next) => {
+
+	try {
+		const refreshToken = req.body.refreshToken
+		
+		if(!refreshToken){
+			return res.status(401);
+		}
+		
+		const verifyToken = jwt.verify(refreshToken, process.env.NODE_JWT_REFRESHTOKEN_KEY);
+
+		if(!verifyToken){
+			return res.status(400);
+		}
+
+		const existUser = await User.findById(verifyToken.id).exec();
+
+		const accessToken = jwt.sign({id:existUser._id}, process.env.NODE_JWT_ACCESSTOKEN_KEY, {expiresIn:86400}); 
+		res.status(200).send({accessToken})
+	
+	} catch (error) {
+		return res.status(400).send('Internal Server Error')
+	}
+}
